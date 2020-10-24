ac71e5fce7688a413e025a6cd39303fe1cea0d11
Sai Kumar <adepusai123@gmail.com>
2020-10-24 14:53:05 +0530

    home page colored

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -27,6 +27,7 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         title: Text('Dashboard'),
@@ -46,7 +47,37 @@ class _HomePageState extends State<HomePage> {
         backgroundColor: Colors.blueAccent,
       ),
       body: Container(
-        child: Text('Welcome to Dashboard. You have logged in successfully.'),
+        height: size.height,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.blueAccent, Colors.tealAccent],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: size.height * 0.2,
+              width: size.width * 0.5,
+              child: Card(
+                color: Colors.teal[100],
+                child: Text(
+                  'Welcome to Dashbord!',
+                  style: TextStyle(
+                    color: Colors.redAccent,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
